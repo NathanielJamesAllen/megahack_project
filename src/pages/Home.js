3c8cdc353250433b35794343d8fcd15a8c9577d4
Noah <noah@pop-os.localdomain>
2024-01-27 17:31:36 -0500

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,45 @@
 // Home.js
 import React from 'react';
 import HomepageImage from '../components/HomepageImage';
+import { Typography, Grid, AppBar, Toolbar, IconButton, Button, TextField} from '@mui/material';
+import DownloadIcon from '@mui/icons-material/Download';
 
 const Home = () => {
     return (
       <>
-          <h1>This is the Home Page</h1>;
-          <HomepageImage />
+          <Typography></Typography>
+          
+
+          <Grid container spacing={2} align="center" justify="center" alignItems="center">
+              <Grid item xs={12} sm={12} md={12}></Grid>
+              <Grid item xs={12} sm={12} md={12}></Grid>
+              <Grid item xs={12} sm={12} md={12}></Grid>
+              <Grid item xs={12} sm={12} md={12}></Grid>
+
+              <Grid item xs={12} sm={12} md={2}></Grid>
+              <Grid item xs={12} sm={12} md={2}></Grid>
+              <Grid item xs={12} sm={12} md={2}>
+                <TextField
+                  id="outlined-multiline-static"
+                  label="Hash-Cracker"
+                  multiline
+                  rows={10}
+                  defaultValue=""
+                />
+              </Grid>
+              <Grid item xs={12} sm={12} md={2}>
+                <Button variant="outlined">Crack Hashes</Button>
+              </Grid>
+              <Grid item xs={12} sm={12} md={2}></Grid>
+              <Grid item xs={12} sm={12} md={2}></Grid>
+              
+              <Grid item xs={12} sm={12} md={12}></Grid>
+              <Grid item xs={12} sm={12} md={12}></Grid>
+              <Grid item xs={12} sm={12} md={12}>
+                <HomepageImage/>
+              </Grid>
+          </Grid>
+
       </>
     );
 }
