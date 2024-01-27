58b41259506046b5f4b85b6e195618ade7523f19
Marcus Moore <marcus4moore@gmail.com>
2024-01-27 15:55:19 -0500

    This is the first commit.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './NavBar';
+import routes from './routes';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+        <Router>
+          <NavBar />
+          <div className="container mt-4">
+            <Routes>
+              {routes.map((route, index) => (
+                <Route key={index} path={route.path} element={route.component} />
+              ))}
+            </Routes>
+          </div>
+        </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+// Home.js
+import React from 'react';
+import HomepageImage from '../components/HomepageImage';
+
+const Home = () => {
+    return (
+      <>
+          <h1>This is the Home Page</h1>;
+          <HomepageImage />
+      </>
+    );
+}
+
+
+export default Home;
